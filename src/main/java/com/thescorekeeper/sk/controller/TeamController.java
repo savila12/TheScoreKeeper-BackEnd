5af87c3630d00e2917753a3669959202053b1d1c
Pete Micsunescu <peat.is.nete@gmail.com>
2021-05-13 16:40:37 -0500

    Created temp endpoint to test teamName return.

--- src/main/java/com/thescorekeeper/sk/controller/TeamController.java
+++ src/main/java/com/thescorekeeper/sk/controller/TeamController.java
@@ -1,5 +1,8 @@
 package com.thescorekeeper.sk.controller;
 
+import com.thescorekeeper.sk.model.Team;
+import com.thescorekeeper.sk.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path="/api")
 public class TeamController {
 
+    private TeamRepository teamRepository;
 
+    @Autowired
+    public void setTeamRepo(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    // Tested endpoint to see if we could grab a team name from database
     @GetMapping("/teams")
     public String getAllTeams() {
         System.out.println("Calling  getAllTeams -->");
-        return "ALL TEAMS";
+        Team team = teamRepository.findByTeamName("bears");
+        String test1 = team.getTeamName();
+        return "here is the team name: " + test1;
     }
 
 
--- src/main/java/com/thescorekeeper/sk/repository/TeamRepository.java
+++ src/main/java/com/thescorekeeper/sk/repository/TeamRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
-
+    Team findByTeamName(String team);
 
 }
